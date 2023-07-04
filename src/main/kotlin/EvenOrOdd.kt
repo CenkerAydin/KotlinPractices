c6a995e7500591145c410af244f8ef92f9b296b1
Cenker Aydın <cenkeraydincse@gmail.com>
2023-07-04 11:57:10 +0300

    Kotlin Practices added

--- src/main/kotlin/EvenOrOdd.kt
+++ src/main/kotlin/EvenOrOdd.kt
@@ -1,3 +1,11 @@
 fun main() {
-    
+    //Program Check Whether a number is even or odd
+    print("Enter the number: ")
+    val num= readLine()!!.toInt()
+    if (num %2==0){
+        print("$num is Even")
+    }else{
+        print("$num is Odd")
+    }
+
 }
